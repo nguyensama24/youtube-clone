aa3628a30098d8066942bee82eccb03880ec19cd
NguyenNVPK02647 <vannguyenhv201@gmail.com>
2024-04-12 12:32:36 +0700

    fix some bug

--- src/components/VideoCmt.jsx
+++ src/components/VideoCmt.jsx
@@ -6,10 +6,17 @@ import { useParams } from 'react-router-dom'
 import { ChannelsCommentCard } from './'
 const VideoCmt = () => {
     const [comments, setComments] = useState([])
+    // const [comment, setComment] = useState(null)
+    // const [authorChannelId, setAuthorChannelId] = useState([])
     const { id } = useParams();
+    // console.log(comments);
     useEffect(() => {
+
         fetchFromAPI(`commentThreads?part=snippet&videoId=${id}&maxResults=50`)
             .then((data) => setComments(data?.items))
+
+        // fetchFromAPI(`comments?part=snippet&id=UgwFtTIpoLvNaz3bcvF4AaABAg`)
+        //     .then((data) => setComment(data.items))
     }, [])
 
 
@@ -17,6 +24,7 @@ const VideoCmt = () => {
         <Stack direction="column" flexWrap="wrap" justifyContent="start" gap={2}>
             {comments.map((item, idx) => (
                 <Box key={idx}>
+
                     {item.snippet.topLevelComment.snippet && <ChannelsCommentCard commentsDetail={item} />}
                 </Box>
             ))}
